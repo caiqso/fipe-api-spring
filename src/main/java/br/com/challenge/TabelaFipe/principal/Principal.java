1c94180210135d8bf66f077e0904a25e5b148daf
Caíque <caaique.c2@gmail.com>
2026-04-29 18:46:01 -0300

    feat: initial release - spring api (Alura challenge)

---
+++ src/main/java/br/com/challenge/TabelaFipe/principal/Principal.java
@@ -0,0 +1,41 @@
+package br.com.challenge.TabelaFipe.principal;
+
+import br.com.challenge.TabelaFipe.service.ConsumoApi;
+
+import java.util.Scanner;
+
+public class Principal {
+
+    private Scanner leitura = new Scanner(System.in);
+
+    private String URL_BASE = "https://parallelum.com.br/fipe/api/v1/";
+
+    private ConsumoApi consumo = new ConsumoApi();
+
+    public void exibeMenu() {
+        var menu = """
+                *** OPÇÕES ***
+                Carro
+                Moto
+                Caminhão
+                
+                Digite uma das opções para consultar: 
+                """;
+
+        System.out.println(menu);
+
+        var opcao = leitura.nextLine();
+        String endereco;
+
+        if (opcao.toLowerCase().contains("car")) {
+            endereco = URL_BASE + "carros/marcas";
+        } else if (opcao.toLowerCase().contains("mot")) {
+            endereco = URL_BASE + "motos/marcas";
+        } else {
+            endereco = URL_BASE + "caminhoes/marcas";
+        }
+
+        var json = consumo.obterDados(endereco);
+        System.out.println(json);
+    }
+}
---
+++ src/main/java/br/com/challenge/TabelaFipe/service/IConverteDados.java
@@ -0,0 +1,5 @@
+package br.com.challenge.TabelaFipe.service;
+
+public interface IConverteDados {
+    <T> T obterDados(String json, Class<T> classe);
+}
