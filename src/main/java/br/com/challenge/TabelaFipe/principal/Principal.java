e602960c9cb219b28839695e4c2370a02a81d429
Caíque <caaique.c2@gmail.com>
2026-04-30 16:25:59 -0300

    refactor: improve ConverteDados and Principal application flow

--- src/main/java/br/com/challenge/TabelaFipe/principal/Principal.java
+++ src/main/java/br/com/challenge/TabelaFipe/principal/Principal.java
@@ -1,16 +1,21 @@
 package br.com.challenge.TabelaFipe.principal;
 
+import br.com.challenge.TabelaFipe.model.Dados;
+import br.com.challenge.TabelaFipe.model.Modelos;
 import br.com.challenge.TabelaFipe.service.ConsumoApi;
+import br.com.challenge.TabelaFipe.service.ConverteDados;
 
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Principal {
 
     private Scanner leitura = new Scanner(System.in);
+    private ConsumoApi consumo = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
 
     private String URL_BASE = "https://parallelum.com.br/fipe/api/v1/";
 
-    private ConsumoApi consumo = new ConsumoApi();
 
     public void exibeMenu() {
         var menu = """
@@ -37,5 +42,23 @@ public class Principal {
 
         var json = consumo.obterDados(endereco);
         System.out.println(json);
+
+        var marcas = conversor.obterLista(json, Dados.class);
+        marcas.stream()
+                .sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
+
+        System.out.println("Qual código da marca que deseja visualizar?");
+        var codigoDaMarca = leitura.nextLine();
+
+        endereco = endereco + "/" + codigoDaMarca + "/modelos";
+        json = consumo.obterDados(endereco);
+        var modeloLista = conversor.obterDados(json, Modelos.class);
+
+        System.out.println("\nModelos dessa marca: ");
+        modeloLista.modelos().stream()
+                .sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
+
     }
 }
