9abbfb2e20c57375a3f73c56c08d12de02fd4dec
Caíque <caaique.c2@gmail.com>
2026-04-30 19:36:23 -0300

    feat: add global exception handling

---
+++ src/main/java/br/com/challenge/TabelaFipe/exception/ApiException.java
@@ -0,0 +1,14 @@
+package br.com.challenge.TabelaFipe.exception;
+
+public class ApiException extends RuntimeException {
+
+    public ApiException(String message) {
+        super(message);
+
+    }
+
+    public ApiException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
---
+++ src/main/java/br/com/challenge/TabelaFipe/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package br.com.challenge.TabelaFipe.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handleApi(RuntimeException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Map.of(
+                        "erro: ", ex.getMessage()));
+    }
+}
